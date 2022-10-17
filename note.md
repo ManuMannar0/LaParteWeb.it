4cbc71686b3ea0c28ab69bdf271d49264e2085a0
Emanuele Mancari <emanuele.mancari@abstract.it>
2022-10-17 19:02:31 +0200

    change site description in js settings

--- note.md
+++ note.md
@@ -1,9 +1,4 @@
-Choose unicode numbers
-
 MenuButtons (mail, regatascript..)
-Lin a corso
-
-
 Better loading/apiReq (please relax in see view video)
-
-Styled use attribute?
+add img hidden seo
+font family better
--- src/THE_BLOG/THE_BLOG_Settings.js
+++ src/THE_BLOG/THE_BLOG_Settings.js
@@ -7,7 +7,7 @@ export const the_blog_menu = 'Menu' /** must be the same of wordpress menu's nam
 export const the_blog_homeButtonName = `laparteweb` /** home button menu */
 // HEAD
 export const the_blog_title = 'La parte web'
-export const the_blog_description = 'La parte codice del web'
+export const the_blog_description = 'The code side of your project'
 // CATEGORIES
 // OTHER
 export const video_bkg = 'waves.mp4'
